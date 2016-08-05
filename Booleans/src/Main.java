a948fcd934062bf695f040e88ec1422f5299b02c
Sorin Diaconescu <diaconescu.sorin.g@gmail.com>
2016-08-04 21:53:23 -0400

    Commit message ONE.

--- Booleans/src/Main.java
+++ Booleans/src/Main.java
@@ -13,20 +13,32 @@ public class Main {
         String s2 = "Hello";
 
         System.out.println("First if statement:");
+        //** The wrong way to compare String values **//
         if (s1 == s2){
-            System.out.println("They match !" + "\n");
+            System.out.println("They match !\n");
         }
         else {
-            System.out.println("They don't match !" + "\n");
+            System.out.println("They don't match !\n");
         }
 
         System.out.println("Second if statement:");
+        //** The right way to compare Strings values.**//
         if (s1.equals(s2)){
-            System.out.println("They match !" + "\n");
+            System.out.println("They match !\n");
         }
         else {
-            System.out.println("They don't match !" + "\n");
+            System.out.println("They don't match !\n");
         }
+
+        //** The wrong way to compare String values. **//
+        if (s1 != s2) {
+            System.out.println("They match !\n");
+        }
+        else {
+            System.out.println("They don't match !\n");
+        }
+
+        //** Write code upt to here **//
     }
 
 }
